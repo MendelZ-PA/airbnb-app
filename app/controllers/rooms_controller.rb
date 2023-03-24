10e69ed9d7b08ed914e154581f3a0a68daf18ba4
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-03-23 22:05:33 -0400

    adds room controller & routes

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,22 @@
+class RoomsController < ApplicationController
+  def create
+    @room = Room.new(
+      user_id: params[:user_id],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      price: params [:price],
+      description: params [:description],
+      home_type: params [:home_type],
+      room_type: params [:room_type],
+      total_occupancy: params [:total_occupancy],
+      total_bedrooms: params [:total_bedrooms], 
+      total_bathrooms: params [:total_bathrooms],
+    )
+    if @room.save
+      render json: { message: "room created"}
+    else
+      render json: { errors: @room.errors.full_messages },status: 422
+    end  
+  end
+end
