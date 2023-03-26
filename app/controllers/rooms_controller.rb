9d5a39a8c70a459fe02a98f4a33381ac745a7437
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-03-26 10:36:55 -0400

    room index/show/create actions

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,25 +3,25 @@ class RoomsController < ApplicationController
     @rooms = Room.all
     render json: @rooms.as_json
   end
-  
+
   def create
     @room = Room.new(
       user_id: params[:user_id],
       address: params[:address],
       city: params[:city],
       state: params[:state],
-      price: params [:price],
-      description: params [:description],
-      home_type: params [:home_type],
-      room_type: params [:room_type],
-      total_occupancy: params [:total_occupancy],
-      total_bedrooms: params [:total_bedrooms], 
-      total_bathrooms: params [:total_bathrooms],
+      price: params[:price],
+      description: params[:description],
+      home_type: params[:home_type],
+      room_type: params[:room_type],
+      total_occupancy: params[:total_occupancy],
+      total_bedrooms: params[:total_bedrooms],
+      total_bathrooms: params[:total_bathrooms],
     )
     if @room.save
-      render json: { message: "room created"}
+      render json: { message: "room created" }
     else
-      render json: { errors: @room.errors.full_messages },status: 422
-    end  
+      render json: { errors: @room.errors.full_messages }, status: 422
+    end
   end
 end
